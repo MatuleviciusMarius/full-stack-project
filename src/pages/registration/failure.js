99cbb3fb7e6a3350fbb1502ff9a9de4a15b2e181
Marius <marius.matulevicius1@gmail.com>
2023-06-02 14:10:44 +0300

    added header and footer to success and failure pag

--- src/pages/registration/failure.js
+++ src/pages/registration/failure.js
@@ -1,5 +1,13 @@
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
 import React from 'react';
 
 export default function Failure() {
-  return <div>Nepavyko</div>;
+  return (
+    <>
+      <Header />
+      Nepavyko
+      <Footer />
+    </>
+  );
 }
--- src/pages/registration/success.js
+++ src/pages/registration/success.js
@@ -1,5 +1,13 @@
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
 import React from 'react';
 
 export default function Success() {
-  return <div>Emailas patvirtintas</div>;
+  return (
+    <>
+      <Header />
+      Emailas patvirtintas
+      <Footer />
+    </>
+  );
 }
