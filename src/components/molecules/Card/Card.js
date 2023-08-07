51997da3f83b1615011bd28e222decacc494b382
Marius <marius.matulevicius1@gmail.com>
2023-08-07 21:09:54 +0300

    reverse card

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -1,24 +1,14 @@
 import React from "react";
 import styles from "./Card.module.css";
 import Link from "next/link";
-import { Card as CardMantine, Text, Image, Group } from "@mantine/core";
 
 export default function Card({ title, text, link, photo }) {
   return (
-    <CardMantine className={styles.card} shadow="sm" padding="lg" radius="md" withBorder>
-      {photo && <Image src={photo} height={"100%"} />}
-      {title && (
-        <Group position="apart" mt="md" mb="xs">
-          <Text weight={600}>{title}</Text>
-        </Group>
-      )}
-
-      {text && <Text ta={"justify"}>{text}</Text>}
-      {link && (
-        <Link className={styles.link} href={link}>
-          Daugiau
-        </Link>
-      )}
-    </CardMantine>
+    <div className={styles.card}>
+      {title && <h2>{title}</h2>}
+      {text && <p className={styles["preserve-line-breaks"]}>{text}</p>}
+      {link && <Link href={link}>Daugiau</Link>}
+      {photo && <img src={photo} />}
+    </div>
   );
 }
