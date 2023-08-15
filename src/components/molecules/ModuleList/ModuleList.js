b81b61385565e40dd09f9c0f418bebafa902c071
Marius <marius.matulevicius1@gmail.com>
2023-08-15 22:39:01 +0300

    update

--- src/components/molecules/ModuleList/ModuleList.js
+++ src/components/molecules/ModuleList/ModuleList.js
@@ -1,8 +1,6 @@
 import LinkToModule from "@/components/atoms/LinkToModule/LinkToModule";
 import React from "react";
 
-// const colorPalette = ["#88E2D3", "#FFB347", "#D94A6A", "#FFE382", "#AEDFF7", "#7EDD80"];
-
 export default function ModuleList({ openModules = 0, userInfo }) {
   const links = [];
   for (let i = 1; i < openModules + 1; i++) {
