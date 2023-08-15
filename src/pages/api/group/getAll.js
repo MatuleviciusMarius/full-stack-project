2db101d1675c81f59d278b51febabcc4d6d04de2
Marius <marius.matulevicius1@gmail.com>
2023-08-15 14:01:39 +0300

    bugfix

--- src/pages/api/group/getAll.js
+++ src/pages/api/group/getAll.js
@@ -1,15 +1,16 @@
-import Group from '@/models/Group';
-import connectMongo from '@/utils/connectMongo';
+import Group from "@/models/Group";
+import User from "@/models/User";
+import connectMongo from "@/utils/connectMongo";
 
 export default async function getAll(req, res) {
   try {
     await connectMongo();
 
-    const groups = await Group.find({}).populate('users');
+    const groups = await Group.find({}).populate("users");
 
     res.status(200).json({ groups });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'server error' });
+    res.status(500).json({ error: "server error" });
   }
 }
