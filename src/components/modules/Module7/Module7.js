c23133f62781469482aaa5264163d70e7a64fbbe
Marius <marius.matulevicius1@gmail.com>
2023-08-15 23:21:25 +0300

    module 7

--- src/components/modules/Module7/Module7.js
+++ src/components/modules/Module7/Module7.js
@@ -1,5 +1,147 @@
-import React from 'react';
+import React from "react";
 
 export default function Module7() {
-  return <div>Module7</div>;
+  return (
+    <div className="color justify-text">
+      <h1 className="center-text">7 MODULIS</h1>
+      <br />
+      <p className="center-text bold">Sveika atvykusi į Norų išsipildymo pasaulį!</p>
+      <br />
+      <p>
+        Tu jau daug padarei, kad tavo norai virstų realybe. Kai noras perteikiamas visur esančiai
+        gyvai mąstančiai protingai substancijai, dar kitaip tą substanciją galima vadinti Visatos
+        sąmone, sekantis žingsnis yra{" "}
+        <b>harmoningo ryšio sukūrimas ir palaikymas su Visatos sąmone</b>. Tai gyvybiškai svarbu.
+        Štai kaip galima tobulai tai padaryti:
+      </p>
+
+      <ol>
+        <li className="bold">
+          Tiki, kad protinga substancija/Visatos sąmonė yra visa ko šaltinis.
+        </li>
+        <li className="bold">Tiki, kad ji duoda viską, ko trokšti.</li>
+        <li className="bold">Jauti substancijai/Visatai gilų ir nuoširdų dėkingumą.</li>
+      </ol>
+
+      <p className="center-text">~~~***~~~</p>
+      <p>
+        Daugumos norai taip ir lieka neišsipildę, nors daug ką ir daro teisingai, nes stokoja
+        dėkingumo.{" "}
+        <b>
+          Sulaukus dovanų iš Visatos ir negebėjus įvertinti, ką gavo, nutraukiamas ryšys su Visata
+        </b>
+        .
+      </p>
+
+      <p className="center-text">~~~***~~~</p>
+      <p>
+        <b>
+          Kuo dažniau nutikus geriems dalykams sutelksi dėmesį į Aukštesniąją jėgą, tuo daugiau ir
+          greičiau gėris tave pasieks.
+        </b>{" "}
+        Ankstesni tavo norai išsipildė ir buvai apdovanota gėrybėmis pagal tam tikrų dėsnių veikimą.
+        Egzistuoja dėkingumo dėsnis. Neįmanomas norų išsipildymas neatsižvelgiant į jį. Dėkingumo
+        dėsnis - tai gamtos dėsnis, kuris teigia, kad veiksmas ir atoveiksmis visada yra vienodai
+        stiprūs ir veikia priešingomis kryptimis. Dėkojimas - tai konkreti žmogaus pastanga, tai
+        konkretus minčių siuntimas, paleidimas į Visatą. Kaip jau žinai, neįmanoma, kad žmogaus
+        pastanga nepasiektų Visatos.
+      </p>
+
+      <p>
+        <b>
+          Kai nuolat jauti stiprų dėkingumą, tai nuolat esi harmoningame ryšyje su Visata ir tau
+          tuomet nuolat siunčiami dalykai, kurių trokšti.
+        </b>{" "}
+        Dėkingas žmogus visuomet būna susikoncentravęs į tai, kas geriausia, tad ir gauna geriausia.
+      </p>
+
+      <p className="margin-bottom-none">
+        Būtina išmokti būti nuoširdžiai dėkingai už kiekvieną gėrį, kuris atsitinka tavo gyvenime.
+        Dėkoti reikia:
+      </p>
+
+      <ol className="margin-top-none">
+        <li>Sau.</li>
+        <li>Žmonėms.</li>
+        <li>Įvykiams, susiklosčiusioms situacijoms.</li>
+        <li>Keturiems elementams, iš ko viskas yra sukurta: žemei, vandeniui, ugniai ir orui.</li>
+        <li>
+          Tam, kas viską kuria, palaiko, organizuoja, išpildo - protingai mąstančiai gyvai
+          substancijai. Ji dar vadinama Visatos sąmone arba viską persmelkenčiu intelektu, Visatos
+          intelektu, Visatos protu, Absoliutu, Šaltiniu.
+        </li>
+      </ol>
+      <p>
+        4 punkto paaiškinimas: Jei pradėsi dėkoti kiekvienai miško gėlytei, kuri tave nudžiugino,
+        kiekvienam medeliui, gražiam debesėliui, žmogui, įvykiams, sau … ir viskam, ką pamatei ar
+        patyrei, kas tave džiugina - to bus per mažai. Yra žymiai daugiau to, kam reikia dėkoti, o
+        tų dalykų tu tiesiog nepamatai, o gal jie per toli, kad pamatytum ar tiesiogiai patirtum.
+        Todėl svarbu dėkoti keturiems elementams, iš ko viskas padaryta (žemei, vandeniui, ugniai,
+        orui). Tokiu būdu tavo dėkingumas bus neaprėpiamai didelis, tiesiog begalinis, ir
+        atitinkamai tokio dydžio atsako sulauksi iš Visatos. Dėkingumo praktika gan paprasta, bet
+        labai galinga. Užsimerkusi vizualizuok šiuos elementus ir kiekvienam padėkok. Tai daryk ryte
+        prabudusi ir vakare prieš užmigdama. Prisiminusi dienos metu, kai matai šiuos elementus,
+        irgi padėkok jiems. Kad tave užlietų gilus ir nuoširdus dėkingumo jausmas, pagalvok apie jų
+        svarbą, kas būtų, jei kurio nors elemento truktų.
+      </p>
+      <p className="margin-bottom-none">
+        5 punkto paaiškinimas: Tiesiog būtina jausti dėkingumą Visatos sąmonei. Taip būsi
+        nuolatiniame harmoningame ryšyje, kas yra būtina norų išsipildymui. Juk neįmanoma nebūti
+        dėkingai tam, kas viską kuria! Dėkojimas Visatai su tavimi gali daryti tikrus stebuklus. Tu
+        gali susijungti su Visatos sąmone. Ši dėkingumo praktika atliekama kasdien, amžinai. Ji
+        kartojama tris kartus per dieną. Daryti reikia taip: rankų plaštakos pirštais į viršų
+        suglaudžiamos krūtinės lygmenyje - Namaste, žvilgsnis nukreipiamas į viršų, tiesiog į
+        kosmosą (geriausia būti lauke arba žiūrėti per langą) nieko konkretaus neieškant ir dėkoti
+        Visatai. Mintyse gali ištarti frazę “Dėkoju už tai, kad laikai mane ir mano žemę”. Tada
+        užsimerkti ir nulenkti galvą kelioms sekundėms. Šią dėkingumo praktiką reikia kartoti tris
+        kartus per dieną:
+      </p>
+      <ol className="margin-top-none">
+        <li>
+          Pirmas kartas ryte, laike nuo saulės patekėjimo iki kol saulė pasieks 30 laipsnių kampą su
+          horizontu. Tai yra maždaug dviejų valandų laiko tarpas.
+        </li>
+        <li>Antras kartas bet kada dienos metu.</li>
+        <li>Trečias kartas vakare, laike nuo saulės nusileidimo per 40 minučių.</li>
+      </ol>
+
+      <p className="center-text">~~~***~~~</p>
+      <p className="center-text bold">Modulio užduotys</p>
+
+      <ol>
+        <li>
+          Parašyk tris svarbiausius dalykus, už ką esi <b>dėkinga sau.</b>
+        </li>
+        <li>
+          Trims <b>žmonėms padėkok.</b> Pati pasirink dėkojimo formą, ar tai darysi gyvai
+          susitikusi, ar vaizdo skambučiu, o gal parašysi padėkos laišką, gal nusiųsi dovanų.
+        </li>
+        <li>
+          Prisimink ir padėkok trims tau <b>palankioms aplinkybėms ar situacijoms</b>, kurioms
+          susiklosčius įvyko tau geri dalykai.
+        </li>
+        <li>
+          Atlikinėk kasdienę dėkojimo praktiką <b>keturiems elementams.</b>
+        </li>
+        <li>
+          Atlikinėk kasdienę dėkojimo praktiką gyvai mąstančiai substancijai/<b>Visatai</b>.
+        </li>
+        <li>
+          Kasdienes dėkojimo praktikas įrašyk į savo užrašinę “8 Man visi linki laimės”. Taip tavo
+          praktinės užduotys bus vienoje vietoje ir tau primins apie save.
+        </li>
+      </ol>
+
+      <p className="center-text">~~~***~~~</p>
+      <p className="center-text bold">
+        Kai nuolat jauti stiprą dėkingumą,
+        <br />
+        tai nuolat esi harmoningame ryšyje su Visata <br />
+        ir tau tuomet nuolat siunčiami dalykai, kurių trokšti
+      </p>
+
+      <p className="center-text">~~~***~~~</p>
+      <p className="center-text">Ačiū!</p>
+    </div>
+  );
 }
