c70c577088d9475ec37f54d22bdbf6e8f5b13969
Marius <marius.matulevicius1@gmail.com>
2023-08-15 23:31:42 +0300

    module 8

--- src/components/modules/Module8/Module8.js
+++ src/components/modules/Module8/Module8.js
@@ -1,5 +1,156 @@
-import React from 'react';
+import React from "react";
 
 export default function Module8() {
-  return <div>Module8</div>;
+  return (
+    <div className="color justify-text">
+      <h1 className="center-text">8 MODULIS</h1>
+      <br />
+      <p className="center-text bold">Sveika atvykusi į Norų išsipildymo pasaulį!</p>
+      <br />
+      <p>
+        Savo likimą galima kurti pačiai. Tam reikia pačiai tapti atsakingai už savo gyvenimą. Karmos
+        ratas negailestingai įsuka į savo ciklą, kuris nuolatos kartojasi, tačiau iš jo galima
+        ištrūkti.
+      </p>
+
+      <p>
+        <b>Karma</b> reiškia veiksmą, bet nebūtinai tai tik fiziniai veiksmai. Tai trijų lygmenų
+        veiksmai - kūno, proto ir energijos. Ką bedarytum šiuose trijuose lygmenyse, tavyje tai
+        palieka pėdsaką. Visi penki tavo jutimo organai kiekvieną akimirką nepaliaujamai renka
+        informaciją iš išorinio pasaulio ir ilgainiui šis didžiulis informacijos srautas tavyje
+        suformuoja tam tikrus savitus modelius. Vėliau šie modeliai virsta į elgesio tendencijas. O
+        šios laikui bėgant tavyje įsitvirtina ir tampa tuo, ką vadiname asmenybe. Tavyje per
+        gyvenimo įvykius ir patirtis kaupėsi įpročiai, polinkiai, tendencijos tau net nesusimąstant
+        apie patį procesą. Karmą galima sulyginti su sena programine įranga, kurią pačios susikūrėme
+        pasąmonėje. Pagal šias programas yra veikiama kūno, proto ir energijos lygmenyse. Tik
+        pagalvok, ką kasdien atnaujini. Tai ir daugybė nereikalingų įpročių, kurie susiformavo dar
+        vaikystėje. Viskas pasikeitė, o tavo ankstesnių patirčių įspaudai tavo pasamonėje ne.
+        Aplinkybės keičiasi, laikas jau kitas, o tavyje cikliškai veikia tie patys modeliai ir
+        mechanizmai. <b>Jei neatsikratysi šių vidinių modelių, niekada nieko naujo neįvyks.</b>
+      </p>
+
+      <p>
+        Viskas, ką darome <b>su kūnu, protu ir energija,</b> mumyse palieka įspaudą. Indijoje šie
+        įspaudai vadinami vasana. Išvertus į lietuvių kalbą tai reiškia kvapą. Kiekvieno žmogaus
+        “kvapas” kuriasi <b>iš fizinių, psichinių, emocinių ir energinių</b> veiksmų sukeltų
+        įspūdžių. Nesusipainiokite, tai ne fizinio kūno kvapas, kurį galima lengvai keisti
+        pakeičiant asmens higienos priemonių kvapą ar parfumus.{" "}
+        <b>
+          Tu į savo gyvenimą pritrauki tokios rūšies gyvenimo situacijas, kokios rūšies kvapą
+          skleidi.
+        </b>{" "}
+        Tai dar vadinama Traukos dėsniu. Jei skleisi vienokią vasaną, tai bus pasirūpinta, kad tam
+        tikru metu atsidurtum vienur, o jei skleisi kitokią vasaną - kitur. Nuo tavo asmeninio kvapo
+        priklauso, ką pritrauki į savo gyvenimą ir ką atitolini.{" "}
+        <b>Kvapas visiškai priklauso nuo tavo karmos turinio.</b>
+      </p>
+
+      <p className="bold margin-bottom-none">
+        Tu gali pasirinkti nebūti visiškai priklausoma nuo savo vasanos/kvapo. Tam reikia
+        sąmoningumo ir atsisakymo. Kaip tai daryti:
+      </p>
+
+      <ol className="bold margin-top-none">
+        <li>
+          Tau nenaudingą įprotį sąmoningai pakeisti kitu. Tu visada turi galimybę rinktis. Ir tai
+          prisimink.
+        </li>
+        <li>O jei turi neįveikiamų potraukių - įsisąmonink juos.</li>
+      </ol>
+
+      <p>
+        Tai kas nutiko tavo gyvenime, nebuvo viskas nulemta iš anksto. Tu pati to nepajausdama
+        susikūrei savo programą ir skleidi tam tikros rūšies kvapą, kuris pritraukia atitinkamas
+        situacijas. Norisi pokyčio? Labai kviečiu.
+      </p>
+      <p className="center-text">~~~***~~~</p>
+      <p>
+        Tavo programinė įranga/karma nėra problema. Bėdų kyla tada, kai ji tampa tavo gyvenimo
+        valdytoja. Tai priklauso nuo tavęs. Karma yra tavo individualumo pagrindas ir tuo pačiu ji
+        yra tavo išankstinių nuostatų pagrindas. Problema yra karmos atmintyje, jei žmogus joje
+        užstringa. Tuomet karma tampa apribojimu. Pateiksiu labai tinkantį pavyzdį su veidrodžiu.
+        Kas atsitiktų su veidrodžiu, jei viskas, kas praeina pro šalį, liptų prie jo. Veidrodis būtų
+        netinkamas. Taip ir tavo suvokimas tampa gerokai aptemdytas.
+      </p>
+
+      <p>
+        Karmos atmintis turi daug privalumų. Ji daro mus nepakartojamais. Bet laikui bėgant ji tampa
+        riba, siena.{" "}
+        <b>
+          Karmos galima atsisakyti dviem būdais: veikiant sąmoningai arba visiškai pasineriant į
+          veiklą.
+        </b>{" "}
+        Jei šiais metodais nesinaudoji, tuomet kaupi karmą. Kai visiškai pasineri į bet kokią veiklą
+        ir pamiršti save, nebelieka ryšio tarp tavęs ir tavo praeities - tavo karma nebeveikia. Tavo
+        veikimo pagrindas turi būti ramybė. Veiksmas, gimęs iš nerimastingumo, ne atgaivina, o
+        sekina, naikina tave.
+      </p>
+
+      <p>
+        Jei mentaliniame lygmenyje nuolat sutelkiamas koks nors saviįvaizdis, jis iš tikrųjų
+        materializuojasi - tam reikia sąmoningų pastangų.
+      </p>
+
+      <p>
+        Kitas būdas kurti ir nereikalaujantis ko nors prašyti ar net galvoti apie tai, nebūtini
+        jokie numatymai, planavimai, schemos. Jei pasirenkamas šitas metodas, viskas, ką reikia
+        daryti, - tai <b>veikti itin intensyviai</b>, nė akimirkos nesvyruojant dėl susitelkimo ir
+        ryžto. O{" "}
+        <b>
+          intensyviai dirbti ir nesijaudinti dėl rezultato galima tik pasiekus palaimos arba meilės
+          būseną.
+        </b>
+      </p>
+
+      <p>Jei būni rami - karma pradeda nykti.</p>
+
+      <p>Jei išlaikai dvasinę pusiausvyrą - prasideda visos karmos struktūros žlugimas.</p>
+
+      <p>
+        Jei aiškiai įsivaizduoji savo naują įvaizdį, šis įvaizdis atgyja tavyje. Jei tavo mintis yra
+        pakankamai galinga ir vizualizacija pakankamai ryški, - tai netgi gali įveikti karmos
+        ryšius. Tai žinodama ir atitinkamai veikdama tampi savo likimo kūrėja.
+      </p>
+      <p className="center-text">~~~***~~~</p>
+
+      <p className="center-text bold">Modulio užduotis</p>
+
+      <p>
+        O dabar pamiršk visus ankstesnius devynis norus. Tris dienas ir tris naktis galvok apie tai,
+        ko tu pati iš tikrųjų nori. Išsigrynink tą vienintelį norą. Jis turi būti toks, kad vien
+        apie jį pagalvojus tau širdis pradeda plakti stipriau. Jis tau pats svarbiausias ir
+        reikšmingiausias. Būk visiškai laisva ir nuoširdi su savimi. Atmesk visus šios tavo
+        akimirkos apribojimus ir esamas ribotas galimybes. Tu esi verta visko, kas yra geriausia.
+        Pamiršk apie kuklumą. Visata tau duoda lygiai tiek, kokio dydžio yra tavo noras. Tą pačią
+        akimirką kai tik išsigrynini <b>savo tikrąjį norą</b>, lai jis virsta ketinimu, nepajudinamu
+        siekiu ir tvirtu tikėjimu, kad taip ir nutiks. Ir tuomet tau bus suteikta tiek jėgų ir
+        galios, kiek jų reikia tavo tikrojo noro išsipildymui.
+      </p>
+      <ol>
+        <li>
+          Parašyk <b>DEŠIMTĄ norą</b> į savo užrašinę “8 Aš visiems linkiu laimės”. Yra sąlyga: jis
+          negali sutapti su anksčiau tavo parašytais norais. Jis tau pats svarbiausias ir
+          reikšmingiausias.
+        </li>
+        <li>
+          Vadovaukis visomis ankstesnių modulių rekomendacijomis ir su šiuo noru atlik visų modulių
+          duotas užduotis.
+        </li>
+        <li>
+          Sukurk ypač ryškią ir įspūdingą Dešimto noro vizualizaciją kaip filmuką. Tai bus tavo
+          gyvenime svarbiausias filmukas. Ji peržiūrinėk nuolatos.
+        </li>
+        <li>
+          <b>Dėkok Visatai kaip už jau išsipildžiusį norą.</b> Įsivaizduok, kad šis noras jau
+          išsipildė ir tave tuomet užlies džiaugsmo banga. Būtent tada ir dėkok Visatai. Tavo
+          dėkingumas tuomet bus nuoširdus.
+        </li>
+        <li>Informuok apie atliktas užduotis.</li>
+      </ol>
+
+      <p className="center-text bold">TROKŠTU TIKRO GYVENIMO</p>
+
+      <p className="center-text">Ačiū!</p>
+    </div>
+  );
 }
