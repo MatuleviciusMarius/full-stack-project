231820a42b1efd0182cab29aef16f770fde90c2e
Marius <marius.matulevicius1@gmail.com>
2023-08-15 22:14:53 +0300

    3 modulis

--- src/components/modules/Module3/Module3.js
+++ src/components/modules/Module3/Module3.js
@@ -1,5 +1,87 @@
-import React from 'react';
+import React from "react";
 
 export default function Module3() {
-  return <div>Module3</div>;
+  return (
+    <div className="color justify-text">
+      <h1 className="center-text">3 MODULIS</h1>
+      <br />
+      <p className="center-text bold">Sveika atvykusi į Norų išsipildymo pasaulį!</p>
+      <br />
+      <p>
+        Keliaujame toliau… Tavo norai greičiausiai labai įvairūs ir ne visų iš karto nori vienu
+        metu. Vienus norus laikai lengvai pasiekiamais, kitiems gal reikia daugiau laiko, o dar kitų
+        taip stipriai nori, bet kažkodėl net negali pradėti eiti link jų. Turbūt esi patyrusi keistą
+        jausmą, kad kažko taip stipriai nori, labai daug svajoji, o noras nė iš vietos, lyg kažkokia
+        aukštesnė jėga laikytų tave už rankų, lyg kažkas neleidžia. Būna ir taip, kad dedi daug
+        pastangų, o finale gaunasi ne taip kaip norėjai arba pasiektas tikslas visai nedžiugina.
+        Tavo neišsipildę norai turi trukdžių. Juos reikia sužinoti. Iš karto nuraminsiu, tai
+        padaryti labai paprasta.
+      </p>
+      <p className="center-text">~~~***~~~</p>
+      <p>
+        Išsirink savo norą iš sąrašo ir atlik vizualizaciją. Įsivaizduok, kad tas noras jau
+        išsipildė (ar jau turi tam tikrą daiktą, ar kažko pasiekei). Stebėk kaip pati jautiesi, ar
+        tau smagu, o gal nejauku. Tada pabandyk įsivaizduoti kaip tau svarbūs žmonės reaguoja į tave
+        su tavo išsipildžiusiu noru. Po to pabandyk įsivaizduoti kaip tu toliau gyvensi su jau
+        išsipildžiusiu noru, kaip keičiasi ar nesikeičia tavo gyvenimas, o kas toliau.
+      </p>
+      <p>Trumpiau tariant:</p>
+      <ol>
+        <li className="bold">“Pasimatuok” norą.</li>
+        <li>Pasižiūrėk kaip pati jautiesi su išsipildžiusiu noru.</li>
+        <li>Pasižiūrėk kaip į tave su tavo išsipildžiusiu noru reaguoja tau svarbūs asmenys.</li>
+        <li>O kas toliau?</li>
+      </ol>
+      <p>
+        Jei buvo smagu vizualizacijos metu, jauteisi ir tu ir tavo aplinkiniai puikiai, tau patiko
+        kokie laukia pokyčiai - viskas gerai.
+      </p>
+      <p>
+        Neigiamos mintys ir jausmai, kurie kilo šio pratimo metu yra trukdžiai noro išsipildymui.
+        Pvz. Vizualizuoji šuolį karjeroje ar versle ir pradeda kilti mintys: jei uždirbsiu daug, tai
+        bus baisu, kad manęs niekas neapvogtų; reiks viską atiduoti mamai (vyrui) ir vis tiek jų
+        neturėsiu; prašys paskolinti; man pavydės, su manimi nebenorės bendrauti ir pan. Kiekviena
+        mintis kyla iš tam tikro įsitikinimo. Jei pagalvojai, kad siekti karjeros bus sunku ir
+        sudėtinga, tai greičiausiai po šia mintimi slypi įsitikinimas, kad karjera - tai sunku ir
+        sudėtinga.
+      </p>
+      <p>
+        Taip vizualizuok visus 9 savo norus ir prie kiekvieno noro parašyk du sąrašus. Viename
+        sąraše kilusias negatyvias mintis, jausmus, kitame - koks po konkrečia mintimi yra tavo
+        įsitikinimas. Pvz. Kilo mintis - neturiu pinigų, tas pirkinys ne man, tai greičiausiai turi
+        neigiamą įsitikinimą apie pinigus (pinigai - tai blogis, visi turtingieji yra apgavikai ir
+        blogi žmonės ir pan.)
+      </p>
+      <p>
+        Atlikus šią užduotį gali pradėti keistis tavo norai. Labai gerai pagalvok apie kiekvieną
+        norą ar tikrai tu pati jo nori. Patikėk,{" "}
+        <b>
+          verta stengtis tik dėl tikrų norų, nes tik jie džiugins iš tikrųjų. Yra didelis skirtumas
+          tarp “Gyventi” ir ”Tikrai gyventi”.
+        </b>
+      </p>
+      <p className="center-text">~~~***~~~</p>
+      <p className="bold center-text">Modulio užduotis</p>
+      <ol>
+        <li>
+          <b>Vizualizuok 9 norus.</b>
+          Kasdien po tris. Geriausia vizualizuoti ten, kur tau niekas netrukdytų. Užsimerki, įeini į
+          meditatyvią būseną ir pradedi vizualizuoti.
+        </li>
+        <li>
+          Pirmas sąrašas - <b>tavo negatyvios mintys ir jausmai</b> vizualizuojant norą.
+        </li>
+        <li>
+          Antras sąrašas - <b>įsitikinimai</b> , kurie slypi po kiekviena negatyvia mintimi iš
+          pirmojo sąrašo.
+        </li>
+        <li>Jei pradėjo keistis tavo norai, juos rašyk savo užrašinėje.</li>
+        <li>Laiku informuok apie atliktas užduotis.</li>
+      </ol>
+      <p className="center-text">~~~***~~~</p>
+      <p className="center-text bold">Verta stengtis tik dėl tikrų norų</p>
+      <p className="center-text">~~~***~~~</p>
+      <p className="center-text">Ačiū!</p>
+    </div>
+  );
 }
