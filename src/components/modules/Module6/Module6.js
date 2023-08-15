282a0e8e0e53554043e3b1c35bee7c46aa2a8084
Marius <marius.matulevicius1@gmail.com>
2023-08-15 23:06:53 +0300

    6 modulis

--- src/components/modules/Module6/Module6.js
+++ src/components/modules/Module6/Module6.js
@@ -1,5 +1,132 @@
-import React from 'react';
+import React from "react";
 
 export default function Module6() {
-  return <div>Module6</div>;
+  return (
+    <div className="color justify-text">
+      <h1 className="center-text">6 MODULIS</h1>
+
+      <p className="center-text bold">Sveika atvykusi į Norų išsipildymo pasaulį!</p>
+
+      <p>
+        <b>Kaip</b> reikia <b>veikti</b>, kad norai išsipildytų? Kad ir kokie būtų tavo veiksmai, tu
+        veikti turi dabar. Negali ką nors padaryti praeityje, tai ir palik praeitį. Taip pat veiksmo
+        negali atlikti ateityje, nes ji dar neatėjo. Tau reikia koncentruotis į dabartį ir daryti
+        tai, ką gali padaryti dabar. Jei dabar esi nepalankioje situacijoje, tai nereiškia, kad tau
+        reikia veiksmus atidėti geresniems laikams. Negaišk laiko galvodama, kaip geriausiai bus
+        galima pasielgti, jei nutiks kas nors blogo ateityje. Jei kas ir nutiks, tai ir susidorosi
+        su bet kokiais ateityje atsirandančiais sunkumais. Tavo protas susipriešina kai veiki dabar,
+        o galvoji apie ateitį.
+      </p>
+
+      <p>
+        Negali atlikti veiksmų ten, kur tavęs nėra. O tavęs nėra nei praeityje, nei ateityje. Vienur
+        jau buvai, kitur dar tik būsi. Nesirūpink ar vakar dienos darbai atlikti gerai ar blogai.
+        Daryk gerai tai, ką gali padaryti šiandien. Nereikėtų bandyti nudirbti ateities darbų. Visa
+        širdimi, visomis jėgomis ir visu protu veik <b>čia ir dabar</b>.
+      </p>
+
+      <p className="center-text">~~~***~~~</p>
+
+      <p>
+        Veikti reikia <b>efektyviai.</b> Tik tokie veiksmai yra sėkmingi. Tu gali kiekvieną savo
+        veiksmą padaryti sėkmingą, nes visos Visatos jėgos nuolat veikia tavo labui. Tau tereikia
+        veiksmus daryti <b>su jėga</b>. Kiekvienas veiksmas yra arba stiprus, arba silpnas. Reikia
+        siekti, kad kiekvienas veiksmas būtų stiprus. Kaip tai padaryti? Atsakymas:{" "}
+        <b>
+          veiksmas sustiprinamas, kai jį atliekant vadovaujiesi savo vizija, veiksmą pripildai savo
+          tikėjimo ir siekio jėgomis
+        </b>
+        . Būtent čia reikia <b>sujungti dvasinę jėgą su fiziniais veiksmais</b>. Jei tam tikru metu
+        naudojiesi dvasinėmis galiomis, o veiksmus palieki visai kitam laikui, patiri nesėkmę.
+        Sėkmingi veiksmai kaupiasi traukdami prie savęs dar daugiau sėkmingų veiksmų. Prisimink, kad
+        viskas Visatoje juda gausesnio link, vis daugiau jėgų prisijungia ir taip norai stiprėja.
+      </p>
+
+      <p className="center-text">~~~***~~~</p>
+      <p>
+        Būtina <b>vaizduotėje susikurti</b> detalią viziją ir apmąstyti sugalvotas detales. Tai
+        daryti reikia kaip galima dažniau, kol detalės įsirėžia į atmintį. Jei nori itin greitų
+        rezultatų, tokiai praktikai skirk kaip galima daugiau laiko. Mintyse nuolat mąstydama apie
+        savo norus suformuoji trokštamą vaizdinį iki smulkiausių detalių.{" "}
+        <b>
+          Į atmintį stipriai įsirėžęs vaizdinys taip pat stipriai yra perduodamas pirminei
+          substancijai.
+        </b>{" "}
+        Vėliau užteks tik akimirkai prisiminti vizualizuotą norą, kaip akimirksniu bus pažadintas
+        tavo tikėjimas ir siekis. O tai motyvuos ir suteiks jėgų atlikti veiksmus geriausiai, kaip
+        gali.
+      </p>
+
+      <p>
+        <b>Kaip efektyviai vizualizuoti norą?</b> <br />
+        Vizualizacija yra galingas įrankis, padedantis norą paversti realybe. Pateikiu
+        efektyviausiais strategijas norų vizualizacijai.
+      </p>
+      <ol>
+        <li>
+          Vizualizuok norą konkrečiai ir detaliai. Galvok apie smulkias detales, įskaitant aplinką,
+          žmones, garsus, kvapus ir emocijas, kurias jausi pasiekusi savo tikslą.
+        </li>
+        <li>Svarbu. Naudok visus penkis pojūčius. Tai pagyvins tavo norimą rezultatą.</li>
+        <li>
+          Sukurk emocinį ryšį. Vizualizacija turi būti ne tik vaizduojanti, bet ir turėti emocinį
+          poveikį. Panagrinėk, kaip jausiesi pasiekusi savo tikslą, ir įsivaizduok tuos jausmus kuo
+          ryškesniais. Ar tai bus džiaugsmas, pasitenkinimas, meilė, laisvė ar kitos teigiamos
+          emocijos, svarbu sukurti stiprų emocinį ryšį su vizualizacija.
+        </li>
+        <li>
+          Sukurk koliažą. Paieškok įkvepiančių paveikslėlių, žodžių ar simbolių, kurie atitinka tavo
+          norą. Sudėliok ir priklijuok juos ant lentelės ar popieriaus. Koliažą pasidėk matomoje
+          vietoje, kad kuo dažniau į jį pasižiūrėtum.
+        </li>
+        <li>
+          Kūrybinis rašymas ar piešimas. Būtinai naudok spalvas, formas ir detales, kurios atitinka
+          tavo norą.
+        </li>
+        <li>
+          Nuotraukų ar vaizdų įrašai. Pasirink įkvepiančias nuotraukas ar vaizdo įrašus, kurie
+          atspindi tavo norą, ir sudėliok juos į skaidres arba sukurk vaizdo prezentaciją. Pridėk
+          muzikos, garsų, įdomių efektų, kad sustiprintum vizualizacijos patrauklumą.
+        </li>
+        <li>
+          Vaizduotės pratimai. Atsigulk ar atsisėsk ramioje vietoje, užmerk akis ir įsivaizduok savo
+          norą kaip gyvą ir realią sceną. Vizualizuok visas smulkias detales su visais pojūčiais.
+          Jei tavo sukurtoje scenoje nebus judesio/veiksmo, taip vaizduotėje sukurtas paveikslėlis
+          greitai dings. Labai svarbu: būtinai vizualizuok judesius. Taip galėsi ilgai išlaikyti
+          norimą vizualizaciją. Tavo vizualizacija turėtų būti kaip filmukas. Pvz. Nori gyventi savo
+          svajonių name. Sudėlioji sceną (vieta, laikas, aplinka, žmonės, smulkios detalės,
+          pojūčiai, …) ir pradedi judinti vaizdus: matai kaip keli savo koją ir statai ant
+          laiptelio, lipi, tiesi ranką atidaryti duris, …, sveikiniesi su žmonėmis, vaikštai po
+          kambarius ir sukinėji galvą apžiūrinėdama juos, įeini į vonios kambarį ir atsuki kraną,
+          bėga vanduo į vonią, tu rengiesi ir lipi į vonią, miegamajame kloji lovą ir t.t. Jei tavo
+          svajonių name yra baseinas, tai vizualizuoji filmuką, kaip lipi į baseiną ir sušlampi, …,
+          kaip švelniu rankšluosčiu šluostaisi. Ar tu nori viena gyventi ten? Būtinai vizualizuok ir
+          judančius žmones su kuriais tu nori gyventi tame name, draugus, šventes, augintinius …
+          Pagrindinis filmuko dalyvis esi tu. Visur matyk save judančią.
+        </li>
+      </ol>
+      <p className="center-text">~~~***~~~</p>
+      <p className="center-text bold">Modulio užduotis</p>
+      <ol>
+        <li>
+          Šiandien pradėk kurti <b>koliažą</b>. Jei jį jau esi sukūrusi, tuomet gali peržiūrėti, gal
+          norėsi ką nors pridėti. Koliažą gali kurti ne tik ant popieriaus, gali ir kompiuterio
+          pagalba.
+        </li>
+        <li>
+          Rytoj <b>aprašyk</b> savo norus. Naudok įvairias spalvas. Pridėk detalių, konkretizuok.
+        </li>
+        <li>
+          Poryt vaizduotėje sukurk išsipildžiusį norą kaip <b>filmuką</b>. Radusi laisvą minutę
+          užmerktomis akimis kartok filmuką, <b>peržiūrinėk jį kaip galima dažniau</b>.{" "}
+        </li>
+        <li>Informuok apie atliktas užduotis.</li>
+      </ol>
+      <p className="center-text">~~~***~~~</p>
+      <p className="center-text bold">Čia ir dabar pagrindinis filmuko dalyvis esi TU</p>
+      <p className="center-text">~~~***~~~</p>
+
+      <p className="center-text">Ačiū!</p>
+    </div>
+  );
 }
